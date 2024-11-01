93a6b8c7a6148ad316d8613d631c7c2ae8e2eb1c
dangkhoa <ddn38@drexel.edu>
2024-11-01 08:45:17 -0400

    moved files

--- app/public/sideBarNav.js
+++ app/public/sideBarNav.js
@@ -1,11 +1,11 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
 }
 
 let cardContainer = document.getElementById("card-container");
@@ -14,41 +14,42 @@ let loader = document.getElementById("loader");
 var throttleTimer;
 
 let throttle = (callback, time) => {
-  if (throttleTimer) return;
+    if (throttleTimer) return;
 
-  throttleTimer = true;
+    throttleTimer = true;
 
-  setTimeout(() => {
-    callback();
-    throttleTimer = false;
-  }, time);
+    setTimeout(() => {
+        callback();
+        throttleTimer = false;
+    }, time);
 };
 
 let createCard = () => {
-  let card = document.createElement("div");
+    let card = document.createElement("div");
 
-  card.className = "card";
-  cardContainer.appendChild(card);
+    card.className = "card";
+    cardContainer.appendChild(card);
 };
 
 let handleInfiniteScroll = () => {
-  throttle(() => {
-    let endOfPage =
-      window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+    throttle(() => {
+        let endOfPage =
+            window.innerHeight + window.pageYOffset >=
+            document.body.offsetHeight;
 
-    if (endOfPage) {
-      createCard();
-    }
-  }, 10);
+        if (endOfPage) {
+            createCard();
+        }
+    }, 10);
 };
 
 let removeInfiniteScroll = () => {
-  loader.remove();
-  window.removeEventListener("scroll", handleInfiniteScroll);
+    loader.remove();
+    window.removeEventListener("scroll", handleInfiniteScroll);
 };
 
 window.onload = function () {
-  createCard();
+    createCard();
 };
 
 window.addEventListener("scroll", handleInfiniteScroll);
