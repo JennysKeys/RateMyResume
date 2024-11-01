1d1ea1e2afd28515430b22325bc38c0d653e6432
dangkhoa <ddn38@drexel.edu>
2024-11-01 09:50:03 -0400

    added card buttons and css

--- app/public/sideBarNav.js
+++ app/public/sideBarNav.js
@@ -1,3 +1,5 @@
+let cardNum = 0;
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -24,10 +26,70 @@ let throttle = (callback, time) => {
     }, time);
 };
 
-let createCard = () => {
-    let card = document.createElement("div");
+let createCardBtn = (icon, bottom, left, id, onClick) => {
+    let button = document.createElement("button");
+    button.className = "card-button";
+    button.id = id;
 
+    let btnIcon = document.createElement("i");
+    btnIcon.className = icon;
+
+    button.appendChild(btnIcon);
+
+    button.style.position = "absolute";
+    button.style.bottom = bottom;
+    button.style.left = left;
+
+    button.addEventListener("click", onClick);
+    return button;
+};
+
+let createCard = (cardNumber) => {
+    let card = document.createElement("div");
     card.className = "card";
+
+    let buttonUp = createCardBtn(
+        "fas fa-arrow-up",
+        "10px",
+        "10px",
+        `buttonUp${cardNumber}`,
+        () => console.log("up button clicked")
+    );
+    let buttonDown = createCardBtn(
+        "fas fa-arrow-down",
+        "10px",
+        "50px",
+        `buttonDown${cardNumber}`,
+        () => console.log("down button clicked")
+    );
+    let buttonComment = createCardBtn(
+        "far fa-comment",
+        "10px",
+        "90px",
+        `buttonComment${cardNumber}`,
+        () => console.log("comment button clicked")
+    );
+    let buttonSave = createCardBtn(
+        "far fa-bookmark",
+        "10px",
+        "130px",
+        `buttonSave${cardNumber}`,
+        () => console.log("save button clicked")
+    );
+    let buttonShare = createCardBtn(
+        "far fa-share",
+        "10px",
+        "170px",
+        `buttonShare${cardNumber}`,
+        () => console.log("share button clicked")
+    );
+
+    card.appendChild(buttonUp);
+    card.appendChild(buttonDown);
+    card.appendChild(buttonComment);
+    card.appendChild(buttonSave);
+    card.appendChild(buttonShare);
+
     cardContainer.appendChild(card);
 };
 
@@ -38,7 +100,9 @@ let handleInfiniteScroll = () => {
             document.body.offsetHeight;
 
         if (endOfPage) {
-            createCard();
+            cardNum += 1;
+            console.log(cardNum);
+            createCard(cardNum);
         }
     }, 10);
 };
