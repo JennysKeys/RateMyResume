8e2345729f62475f46fe8949155550aa5375f0d2
Jenny <wjenny003@gmail.com>
2024-11-07 16:10:50 -0500

    Created a pool using the parameters from the .env file

--- app/server.js
+++ app/server.js
@@ -3,10 +3,28 @@ const app = express();
 
 const port = 3000;
 const hostname = "localhost";
+const dotenv = require("dotenv").config();
 
 app.use(express.static("public"));
 app.use(express.json());
 /* YOUR CODE HERE */
+
+const {PGHOST, PGDATABASE, PGUSER, PGPASSWORD} = process.env;
+
+const pool = new Pool({
+    host: PGHOST,
+    database: PGDATABASE, 
+    username: PGUSER,
+    password: PGPASSWORD,
+    port: 5432,
+    ssl: {
+        require: true,
+    }
+})
+
+app.get("/", async() => {
+
+})
 
 app.listen(port, hostname, () => {
     console.log(`Listening at: http://${hostname}:${port}`);
