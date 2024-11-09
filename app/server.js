2c79e225480de5d6bb910416a124244c56da6aac
Jenny <wjenny003@gmail.com>
2024-11-09 04:23:13 -0500

    Added get request to grab user information from database.

--- app/server.js
+++ app/server.js
@@ -4,7 +4,10 @@ const app = express();
 const port = 3000;
 const hostname = "localhost";
 const dotenv = require("dotenv").config();
+const cors = require("cors");
+const { Pool } = require("pg");
 
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 /* YOUR CODE HERE */
@@ -22,8 +25,24 @@ const pool = new Pool({
     }
 })
 
-app.get("/", async() => {
+app.get("/database", async(req, res) => {
 
+    console.log("connected");
+    const client = await pool.connect();
+
+    try{
+
+        const result = await pool.query("SELECT * FROM users");
+
+        console.log(result.rows);
+        res.json(result.rows);
+    } catch(error) {
+        console.log(error);
+    } finally {
+        client.release()
+    }
+
+    res.status(404);
 })
 
 app.listen(port, hostname, () => {
