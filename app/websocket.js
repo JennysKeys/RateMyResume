fc69ebeb0d3b3ec250125e0ac186cc5c87e9af0c
dangkhoa <ddn38@drexel.edu>
2024-12-08 19:00:09 -0500

    websocket fix attempt

--- app/websocket.js
+++ app/websocket.js
@@ -1,44 +1,49 @@
 const WebSocket = require("ws");
+const http = require("http");
 const url = require("url");
-const port = 443;
 
-let server;
+const port = 8080; // Use port 8080 for local development
 
-function startWebSocketServer() {
-    server = new WebSocket.Server({ port: port });
-    server.on("connection", (ws, req) => {
-        console.log("Client connected");
-        let queryParams = url.parse(req.url, true).query;
-        let userID = queryParams.userID;
-        ws.userID = userID;
+const server = http.createServer();
 
-        ws.on("message", function incoming(message) {
-            let parsedMessage = JSON.parse(message);
-            server.clients.forEach((client) => {
-                if (
-                    client.readyState === WebSocket.OPEN &&
-                    client.userID === parsedMessage.receiverID
-                ) {
-                    client.send(parsedMessage);
-                }
-            });
-        });
-        ws.on("close", function () {
-            console.log("Client disconnected");
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws, req) => {
+    console.log("Client connected");
+    let queryParams = url.parse(req.url, true).query;
+    let userID = queryParams.userID;
+    ws.userID = userID;
+
+    ws.on("message", function incoming(message) {
+        let parsedMessage = JSON.parse(message);
+        wss.clients.forEach((client) => {
+            if (
+                client.readyState === WebSocket.OPEN &&
+                client.userID === parsedMessage.receiverID
+            ) {
+                client.send(parsedMessage);
+            }
         });
     });
+
+    ws.on("close", () => {
+        console.log("Client disconnected");
+    });
+});
+
+server.listen(port, () => {
     console.log(`WebSocket server running on port ${port}`);
+});
+
+function startWebSocketServer() {
+    console.log("WebSocket Server started");
 }
 
 function broadcastMessage(message) {
     console.log(message);
     console.log("hello");
 
-    if (!server) {
-        startWebSocketServer();
-    }
-
-    server.clients.forEach((client) => {
+    wss.clients.forEach((client) => {
         console.log(client.userID);
         if (
             client.readyState === WebSocket.OPEN &&
